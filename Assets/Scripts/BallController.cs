1480fd68dde1dd5c9dece9a571e7ad3d1384bf98
Adam Zabenskie <zubs047@gmail.com>
2025-01-28 13:00:17 -0800

    input manager

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,38 +2,13 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public Rigidbody sphereRigidBody;
-    public float ballSpeed = 2f;
+    [SerializeField] private Rigidbody sphereRigidBody;
+    [SerializeField] private float ballSpeed = 2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void moveBall(Vector2 input)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 inputVector = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector += Vector2.up;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector += Vector2.down;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector += Vector2.right;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector += Vector2.left;
-        }
-
-        Vector3 inputXZPlane = new(inputVector.x, 0, inputVector.y);
+        Vector3 inputXZPlane = new(input.x, 0, input.y);
         sphereRigidBody.AddForce(inputXZPlane * ballSpeed);
     }
 }
