3ec815a4b11ad0a73c673c71e5601a2a40b93f49
Adam Zabenskie <zubs047@gmail.com>
2025-01-28 19:57:29 -0800

    added jumping

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,11 +4,33 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidBody;
     [SerializeField] private float ballSpeed = 2f;
+    [SerializeField] private float ballJumpForce = 1f;
+    private bool isGrounded = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void moveBall(Vector2 input)
+    public void moveBall(Vector3 input)
     {
-        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        Vector3 inputXZPlane = new(input.x, 0, input.z);
+        Vector3 inputYPlane = new(0, input.y, 0);
         sphereRigidBody.AddForce(inputXZPlane * ballSpeed);
+        if (isGrounded)
+        {
+            sphereRigidBody.AddForce(inputYPlane * ballJumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,7 +3,7 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-    public UnityEvent<Vector2> onMove = new UnityEvent<Vector2>();
+    public UnityEvent<Vector3> onMove = new UnityEvent<Vector3>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,23 +14,27 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero;
+        Vector3 inputVector = Vector3.zero;
 
         if (Input.GetKey(KeyCode.W))
         {
-            inputVector += Vector2.up;
+            inputVector += Vector3.forward;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            inputVector += Vector2.down;
+            inputVector += Vector3.back;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            inputVector += Vector2.right;
+            inputVector += Vector3.right;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            inputVector += Vector2.left;
+            inputVector += Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inputVector += Vector3.up;
         }
 
         onMove?.Invoke(inputVector);
